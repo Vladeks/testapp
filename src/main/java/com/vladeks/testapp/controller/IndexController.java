ed9529b4dba757a1d08291aba5ff967228999942
Vladeks <gerogo333@gmail.com>
2019-04-12 19:12:05 +0300

    database settings

--- src/main/java/com/vladeks/testapp/controller/IndexController.java
+++ src/main/java/com/vladeks/testapp/controller/IndexController.java
@@ -1,12 +1,14 @@
 package com.vladeks.testapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
+    @ResponseBody
     public String index() {
         return "Running...";
     }
