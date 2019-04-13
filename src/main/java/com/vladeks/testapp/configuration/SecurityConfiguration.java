1221cd936bc33f6db049d64429572f769137a0f9
Vladeks <gerogo333@gmail.com>
2019-04-13 16:39:40 +0300

    init remote server

--- src/main/java/com/vladeks/testapp/configuration/SecurityConfiguration.java
+++ src/main/java/com/vladeks/testapp/configuration/SecurityConfiguration.java
@@ -30,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .password("user")
                 .roles("USER")
                 .build();
+
         return new InMemoryUserDetailsManager(admin, user);
     }
 
